30d69d8d33c7008698875e052739f37e50d23fd5
Section One <b@b.es>
2024-06-23 16:01:49 +0200

    Codi JC13 - Arrodonint imatges i altres efectes en Jetpack Compose

--- app/src/main/java/es/cibernarium/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/es/cibernarium/jetpackcomposeapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +23,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -47,14 +50,9 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(15.dp)
                     ) {
 
-                        Image(
-                            painter = painterResource(R.drawable.qx7jysym_400x400),
-                            contentDescription = "Logo Refus",
-                            modifier = Modifier.height(100.dp)
-                        )
+                        Avatar()
                         SalutacioPersonal(nom = "Francesc")
-                        Text(text = "Benvingut a la teva App de montanyisme i a on trobaràs tots els recursos a la teva disposició.")
-
+                        Description()
                     }
                 }
             }
@@ -71,5 +69,17 @@ class MainActivity : ComponentActivity() {
             fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.Serif
         )
+    }
+    @Composable
+    private fun Avatar(){
+        Image(
+            painter = painterResource(R.drawable.qx7jysym_400x400),
+            contentDescription = "Logo Refus",
+            modifier = Modifier.height(100.dp).clip(CircleShape).background(Color.Blue)
+        )
+    }
+    @Composable
+    private fun Description(){
+        Text(text = "Benvingut a la teva App de montanyisme i a on trobaràs tots els recursos a la teva disposició.")
     }
 }
