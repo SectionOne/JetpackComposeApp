d82a2dbc8d8d634f5a40b113d09eb71d387fd5b2
Section One <b@b.es>
2024-06-23 13:30:12 +0200

    Codi JC3 - Text i les seves propietats en Jetpack Compose

--- app/src/main/java/es/cibernarium/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/es/cibernarium/jetpackcomposeapp/MainActivity.kt
@@ -6,20 +6,40 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import es.cibernarium.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             JetpackComposeAppTheme {
-
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Tot el que posem aqui serà la part visible de la nostre Aplicació
+                    Text(
+                        text = "Hello world!",
+                        color = Color.Red,
+                        fontSize = 18.sp,
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Serif
+                    )
+                }
             }
         }
     }
