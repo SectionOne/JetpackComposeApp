f576e24313aa89438a5bd68bc4a976e531faf9c5
Section One <b@b.es>
2024-06-24 13:05:19 +0200

    Codi JC24 Bis - Navegació creant la primera pantalla en Jetpack Compose

--- app/src/main/java/es/cibernarium/jetpackcomposeapp/pantalles/PrimeraPantalla.kt
+++ app/src/main/java/es/cibernarium/jetpackcomposeapp/pantalles/PrimeraPantalla.kt
@@ -1,18 +1,22 @@
 package es.cibernarium.jetpackcomposeapp.pantalles
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun PrimeraPantalla(){
-    Scaffold {
+    //Scaffold (
         ContingutCos()
-    }
+    //}
 }
 
 @Composable
@@ -23,9 +27,4 @@ fun ContingutCos(){
             Text("Navega")
         }
     }
-}
-
-@Composable
-fun VistaPrevia(){
-    PrimeraPantalla()
 }
